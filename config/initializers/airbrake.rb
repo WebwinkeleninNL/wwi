423c779f5d01dedca46a2db0f86e2160f4d9fcfb
Tsyren Ochirov <nsu1team@gmail.com>
2014-04-10 23:40:26 +0200

    airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,7 @@
+
 Airbrake.configure do |config|
-  config.api_key = 'hjiokjn5671c180065d8'
-  config.host    = 'stage.webwinkelenin.nl'
+  config.api_key = '45ce6527963775ef500d6ec8ec7b10fb'
+  config.host    = 'errbit.webwinkelenin.nl'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
